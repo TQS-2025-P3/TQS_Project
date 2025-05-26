2fef249045f4223998baa76c18330c5349a46fa1
João <j.morais@ua.pt>
2025-05-26 04:53:29 +0100

    added cucumber testing

---
+++ projX/src/test/java/tqs/project/CucumberTest.java
@@ -0,0 +1,14 @@
+package tqs.project;
+
+import io.cucumber.junit.platform.engine.Constants;
+
+import org.junit.platform.suite.api.*;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "tqs.project.steps")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty, json:target/surefire-reports/cucumber-report.json")
+public class CucumberTest {
+
+}
