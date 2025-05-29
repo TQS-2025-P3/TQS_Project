94243c75501a150aaff7c1766ba4712ddd1952cb
João <j.morais@ua.pt>
2025-05-29 21:10:32 +0100

    changes to dockerfile

--- projX/init.sql
+++
@@ -1,5 +0,0 @@
-DROP USER IF EXISTS 'Evera_staff'@'localhost';
-
-CREATE USER IF NOT EXISTS 'Evera_staff'@'%' IDENTIFIED BY 'senha1234';
-GRANT ALL PRIVILEGES ON Evera.* TO 'Evera_staff'@'%';
-FLUSH PRIVILEGES;
