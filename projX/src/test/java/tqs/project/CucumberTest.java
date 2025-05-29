55e2eca2fa7a8cc87f6038cf0e6b1d773522741c
João <j.morais@ua.pt>
2025-05-29 19:54:39 +0100

    fix

--- projX/src/test/java/tqs/project/CucumberTest.java
+++ projX/src/test/java/tqs/project/CucumberTest.java
@@ -8,7 +8,7 @@ import org.junit.platform.suite.api.*;
 @IncludeEngines("cucumber")
 @SelectClasspathResource("features")
 @ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "tqs.project.steps")
-@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = Constants.PLUGIN_PROPERTY_NAME, value = "pretty, junit:target/surefire-reports/TEST-tqs.project.CucumberTest.xml")
 public class CucumberTest {
 
 }
