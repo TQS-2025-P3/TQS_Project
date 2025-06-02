98411e6a0a5d4a25291e955e61a72139ec084b21
João <j.morais@ua.pt>
2025-06-02 18:43:22 +0100

    added contextLoads as a test issue

--- projX/src/test/java/tqs/project/NikEvApplicationIT.java
+++ projX/src/test/java/tqs/project/NikEvApplicationIT.java
@@ -3,6 +3,9 @@ package tqs.project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import app.getxray.xray.junit.customjunitxml.annotations.XrayTest;
+
+@XrayTest(key = "TQSPROJECT-469")
 @SpringBootTest
 class NikEvApplicationIT {
 
