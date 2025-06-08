4a4ddbd9e4e5545069cc0c3ac4ee2272989c7a78
Gon52 <gonabrantes52@gmail.com>
2025-06-08 20:16:28 +0100

    fixed

--- projX/src/main/java/tqs/project/controller/StatisticsController.java
+++ projX/src/main/java/tqs/project/controller/StatisticsController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:8080", "http://localhost:3306"})
 public class StatisticsController {
 
     @Autowired
